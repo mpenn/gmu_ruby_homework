765147955c37c30d6bdadfec1a3a8ecc7a135160
mpenn <matt.w.penn@gmail.com>
2015-04-25 14:58:28 -0700

    Finished Ruby HW

--- functions.rb
+++ functions.rb
@@ -33,3 +33,8 @@ def fizzbuzz
   end
   return a
 end
+
+puts reverse([3,6,'dog']).inspect
+puts histogram('The Quick brown fox').inspect
+puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
+puts fizzbuzz.join("\n")
